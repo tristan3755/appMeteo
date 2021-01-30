d293078a378fd72665bfc574ab074cb5d9cb8438
FIRST_tristan LAST_richard <tristan3755@gmail.com>
2021-01-30 18:58:40 +0100

    meteo

--- javascript.js
+++ javascript.js
@@ -4,7 +4,8 @@ function maRequeteNancy() {
     if (this.readyState == 4 && this.status == 200) {
       let infoMeteoNancy = JSON.parse(this.response);
       nomVille(infoMeteoNancy);
-      //console.log(infoMeteoNancy)
+      temps(infoMeteoNancy);
+      console.log(infoMeteoNancy)
     }
   };
   xhttp.open(
@@ -19,13 +20,74 @@ maRequeteNancy();
 
 function nomVille(nomVille) {
   let ville = document.getElementById("ville");
+  let bG=document.getElementById('background')
   let nomDeVille = document.createElement("h1");
   nomDeVille.classList= ('titreVille')
 
   for (let i in nomVille) {
-    nomDeVille.innerHTML = nomVille.name;
-    console.log(nomVille.name);
+    let lieu=nomDeVille.innerHTML = nomVille.name;
+    //console.log(lieu);
+
+    if(lieu=="Arrondissement de Nancy"){
+      bG.style.backgroundImage="url('./ressources/images/nancy.jpg')"
+      bG.style.backgroundSize='cover'
+      bG.style.backgroundPosition="center"
+      bG.style.filter='grayscale(40%)'
+    }
+
   }
 
   ville.appendChild(nomDeVille);
+}
+
+function temps(donnees){
+
+  let blocTemps=document.querySelector('.ciel')
+let descriptionTemps=document.createElement('p')
+descriptionTemps.style.fontFamily=" meteo"
+descriptionTemps.style.fontSize="1.8vw"
+descriptionTemps.style.color="whitesmoke"
+descriptionTemps.style.fontWeight="bold"
+descriptionTemps.style.filter="drop-shadow(2px 2px black)"
+
+let imageTemps=document.createElement('img')
+imageTemps.style.width="300px"
+imageTemps.style.maxWidth="100%"
+imageTemps.style.height="auto"
+
+let vent=document.createElement('p')
+vent.style.fontFamily=" meteo"
+vent.style.fontSize="1.8vw"
+vent.style.color="whitesmoke"
+vent.style.fontWeight="bold"
+vent.style.filter="drop-shadow(2px 2px black)"
+
+blocTemps.appendChild(descriptionTemps)
+blocTemps.appendChild(imageTemps)
+blocTemps.appendChild(vent)
+
+
+  for (let i in donnees.weather){
+
+    let meteorologie=donnees.weather[i].main
+
+    if(meteorologie==="Drizzle" || meteorologie==="Mist"){
+      descriptionTemps.innerHTML="Temps brumeux et pleuvieux"
+      imageTemps.src="./ressources/images/nuage.png"
+    
+    }else if(meteorologie==="Clouds"){
+      descriptionTemps.innerHTML="Temps nuageux"
+      imageTemps.src="./ressources/images/nuage.png"
+    }
+
+console.log(meteorologie)
+  }
+
+  for (let i in donnees.wind){
+
+    vent.innerHTML="le vent souffle à : "+donnees.wind.speed * 1.61+' km/h'
+
+  }
+
+
 }
